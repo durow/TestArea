b55858cdef2fbb03ca7a2783a4679ff7b6ceb7ca
durow <durow@163.com>
2015-09-30 14:14:09 +0800

    漏掉的文件，shit

---
+++ MVVMTest/MessageTest/MainWindowViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using MyMVVM;
+
+namespace MessageTest
+{
+    class MainWindowViewModel:NotifyObject
+    {
+
+        private MyCommand _msgTestCommand;
+
+        public MyCommand MsgTestCommand
+        {
+            get
+            {
+                if (_msgTestCommand == null)
+                    _msgTestCommand = new MyCommand(
+                        new Action<object>(o =>
+                        {
+                            MessageManager.Default.SendMsg("ShowBox", "测试测试");
+                        }));
+                return _msgTestCommand;
+            }
+        }
+    }
+}
---
+++ MVVMTest/MyMVVM/MsgActionInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyMVVM
+{
+    public class MsgActionInfo
+    {
+        public object RegInstance { get; internal set; }
+        public string MsgName { get; internal set; }
+        public string Group { get;  internal set; }
+        public Action Action { get; internal set; }
+
+        public void Execute()
+        {
+            if (Action != null)
+                Action();
+        }
+    }
+}
